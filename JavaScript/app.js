ea8f6fe7a7a6b5f7b022c2ffcd88cb6275309cc3
Javohir708 <joxauzking@gamil.com>
2024-12-13 20:00:41 +0500

    API dan ma'lumotlar olinadi users qisminiki

---
+++ JavaScript/app.js
@@ -0,0 +1,33 @@
+const wrapperEl = document.querySelector(".hero__wrapper")
+
+const BASE_URL = "https://dummyjson.com"
+
+async function fetchData(endpoint) {
+    const response = await fetch(`${BASE_URL}${endpoint}`)
+    response
+    .json()
+    .then((res) => createCard(res))
+
+}
+
+window.addEventListener("load", () => {
+    fetchData("/users")    
+})
+
+function createCard (data) {    
+
+    data.users.forEach(user => {
+        const divEl = document.createElement("div")
+        divEl.className = "hero__card"
+        divEl.innerHTML = `
+            <div class="hero__image">
+                <img src="${user.image}" alt="users image">
+            </div>
+            <h2>${user.firstName} ${user.lastName}</h2>
+            <p>age: ${user.age}</p>
+            <strong>Phone: ${user.phone}</strong>
+            <button class="hero__btn">Contact</button>
+        `
+        wrapperEl.appendChild(divEl)
+    });
+}
